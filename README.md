ca2776bb529130903383a1c605186b0f2dc3a8ed
Freewind <nowindlee@gmail.com>
2021-01-30 21:58:44 +0800

    typescript-react-component-find-all-children-demo

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
-TypeScript React Hello World Demo
+TypeScript React Component Find All Children Demo
 =================================
+
+怎么寻找一个react component的所有children.
+
+关键点在于`.props.children`和`React.Children`提供的工具方法
 
 ```
 npm install
 npm run demo
 ```
-
-It will open page on browser automatically.
--- src/hello.tsx
+++ src/hello.tsx
@@ -1,9 +1,29 @@
-import React, { FC } from 'react';
+import React, {FC, ReactNode} from 'react';
 
 type Props = {};
 
+function getChildren(node: React.ReactNode): ReactNode[] {
+  if (typeof node === 'object') {
+    return React.Children.toArray((node as any)?.props?.children);
+  }
+  return [];
+}
+
+function findAllChildren(element: React.ReactNode): React.ReactNode[] {
+  const allChildren: React.ReactNode[] = [...React.Children.toArray(element)]
+  for (let i = 0; i < allChildren.length; i += 1) {
+    const child = allChildren[i];
+    allChildren.push(...getChildren(child))
+  }
+  return allChildren;
+}
+
 export const Hello: FC<Props> = ({}) => {
-  return <div>
+  const element = <div>
     <h1>Hello React</h1>
+    <h2>
+      <button>Click</button>
+    </h2>
   </div>;
+  return <>{findAllChildren(element).map(it => <div style={{border: '1px solid red', margin: 10}}>{it}</div>)}</>
 }
